2a93f8bc5a4b4f640117344d3505140cb807cedb
Antti <anttis.pitkanen@gmail.com>
2016-12-12 13:38:08 +0200

    mobile browser url bar bug fixed

--- client/home/Banner.jsx
+++ client/home/Banner.jsx
@@ -1,6 +1,29 @@
 import React, {Component} from 'react';
 
+
+
 export default class Banner extends Component {
+
+    componentDidMount() {
+        let height = window.innerHeight;
+        document.getElementById("home").style.height = `${height}px`;
+
+        //let's only resize the height if it changes by more than HEIGHT_DIFFERENCE_THRESHOLD = 60
+        //this prevents the mobile browser url bar bug but doesn't prevent resizing e.g. when rotating phone
+        window.addEventListener("resize", () => {
+
+            let newHeight = window.innerHeight;
+            let heightDifference = Math.abs(height - newHeight);
+
+            const HEIGHT_DIFFERENCE_THRESHOLD = 60;
+
+            if (heightDifference > HEIGHT_DIFFERENCE_THRESHOLD) {
+                height = newHeight;
+                document.getElementById("home").style.height = `${height}px`;
+            }
+        });
+    }
+
 
     render() {
         return(
