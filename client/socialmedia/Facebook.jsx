4828d2440c45de2bf2e73928af4276950f5c78d2
Antti <anttis.pitkanen@gmail.com>
2016-12-12 11:10:16 +0200

    switched facebook text and video the other way around

--- client/socialmedia/Facebook.jsx
+++ client/socialmedia/Facebook.jsx
@@ -6,8 +6,6 @@ export default class Facebook extends Component {
         return(
             <div id="facebook" className="facebook clearfix">
 
-                <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fpoetkoemusic%2Fvideos%2F654563564691379%2F&show_text=0&width=560" width="560" height="315" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
-
                 <div className="facebook-text">
                     <h2><a href="https://www.facebook.com/poetkoemusic" target="_blank"><i className="fa fa-facebook-official"></i> Facebook</a></h2>
                     <p>
@@ -16,6 +14,8 @@ export default class Facebook extends Component {
                     </p>
                     <p><a href="https://www.facebook.com/poetkoemusic" target="_blank">Wow, such content, much exclusive!</a></p>
                 </div>
+
+                <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fpoetkoemusic%2Fvideos%2F654563564691379%2F&show_text=0&width=560" width="560" height="315" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
             </div>
         )
     }
