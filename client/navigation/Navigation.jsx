63be16577cc38a5eb969526e9fb3dbc679bcb504
Antti <anttis.pitkanen@gmail.com>
2016-12-13 17:19:14 +0200

    changed introduction embed

--- client/navigation/Navigation.jsx
+++ client/navigation/Navigation.jsx
@@ -1,18 +1,72 @@
 import React from 'react';
 
+//let ypos = 0; //to keep track of window top postion for scroll animation
+
 export default class Navigation extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            menuOpen: false
+        };
+    }
+/*
+    componentDidMount() {
+
+        window.addEventListener('scroll', () => {
+            ypos = window.scrollY;
+        });
+
+        requestAnimationFrame(this.animateHamburger);
+    }
+
+    animateHamburger() {
+        let hamburger = document.getElementById('menu-icon-toggle');
+
+        //requestAnimationFrame(Navigation.animateHamburger);
+
+        let top = ypos / 3;
+        hamburger.style.transform = `rotate(${top}deg)`;
+    }
+*/
+
+
+
+//    componentDidUpdate() {
+        /*
+        if (document.getElementById('main-navigation').classList.contains("main-navigation-showing")) {
+            document.documentElement.onclick = () => {
+                document.getElementById('main-navigation').classList.toggle("main-navigation-showing");
+            }
+        }
+        */
+/*
+        console.log(this.state.menuOpen);
+
+        if (this.state.menuOpen) {
+            document.documentElement.onclick = () => {
+                document.getElementById('main-navigation').classList.toggle("main-navigation-showing");
+            }
+            this.setState( { menuOpen: false } );
+        }
+    }
+*/
 
     //click at the hamburger menu icon
     toggleNavigationShowing() {
         document.getElementById('main-navigation').classList.toggle("main-navigation-showing");
+        this.setState( { menuOpen: !this.state.menuOpen } );
     }
 
     //closes the navigation dropdown when a list item is clicked
     closeNavigationOnClick() {
         document.getElementById('main-navigation').classList.toggle("main-navigation-showing");
+        this.setState( { menuOpen: !this.state.menuOpen } );
     }
 
+
     render() {
+
         return(
             <nav className="clearfix">
 
@@ -24,11 +78,11 @@ export default class Navigation extends React.Component {
                     <li><a href="https://www.instagram.com/poetkoe" target="_blank"><i className="fa fa-instagram"></i></a></li>
                 </ul>
 
-                <div id="menu-icon" onClick={this.toggleNavigationShowing} className="menu-icon">
+                <div id="menu-icon" onClick={this.toggleNavigationShowing.bind(this)} className="menu-icon">
                     <i id="menu-icon-toggle" className="fa fa-bars"></i>
                 </div>
 
-                <ul id="main-navigation" onClick={this.closeNavigationOnClick} className="main-navigation">
+                <ul id="main-navigation" onClick={this.closeNavigationOnClick.bind(this)} className="main-navigation">
 
                     <a href="#home"><li>Home</li></a>
                     <a href="#soundcloud"><li>Music</li></a>
